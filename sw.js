cd56e7a98e163f67abd6d8b563343f0fc83bbcde
WritwikRay <writwikray@gmail.com>
2022-09-22 18:40:03 +0530

    added a new post

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_DATE = "2022-09-21";   const BASE_CACHE_FILES = [
+const BUILD_DATE = "2022-09-22";   const BASE_CACHE_FILES = [
     '/css/bundle.min.css',
     '/js/bundle.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js',
